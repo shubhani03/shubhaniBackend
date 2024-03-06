6d8423b3f0e3f92d7fb1e74c4e60608117533821
shubhani03 <162417967+shubhani03@users.noreply.github.com>
2024-03-06 16:50:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const AdminRouters = require("./src/routers/AdminRouters.js");
+const dbConnect = require("./src/config/config.js");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
 app.use(express.json());
 app.use(cors());
 dotenv.config();
-const port = process.env.REACT_APP_PORT;
+
+const port = process.env.REACT_APP_PORT || 8080;
+
+app.listen(port, async () => {
+  await dbConnect();
+
+  console.log("backend server running on port no : ", port);
+});
 app.get("/", (req, res) => {
   res.status(200).send({
     message: "Welcome to backend server in localhost",
